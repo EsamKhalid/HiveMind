cb2effe7e597043f7529a485da183302acaeff6d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2024-11-27 20:37:52 +0000

    IS THAT A THREAT?!

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('home');
@@ -22,7 +24,29 @@ Route::get('basket', [TestController::class, 'basket'])->name('basket');
 
 Route::get('about', [TestController::class, 'about'])->name('about');
 
-Route::post('/generate-password', function (\Illuminate\Http\Request $request) 
+// 27/11/24 - NOT SURE IF THIS IS NEEDED, LEAVING HERE JUST IN CASE
+// DO NOT TOUCH THIS, OR OFF WITH YOUR HEAD
+
+Route::post('/generate-password', function (Request $request) 
 {
-    require_once base_path('phpPass/password.php');
+    $length = $request->input('length', 16);
+    $hasUpper = $request->input('hasUpper', true);
+    $hasLower = $request->input('hasLower', true);
+    $hasNums = $request->input('hasNums', true);
+    $hasSyms = $request->input('hasSyms', true);
+
+    $characters = '';
+    if ($hasUpper) $characters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if ($hasLower) $characters .= 'abcdefghijklmnopqrstuvwxyz';
+    if ($hasNums) $characters .= '0123456789';
+    if ($hasSyms) $characters .= '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    if (empty($characters)) {
+        return response()->json(['error' => 'No character types selected'], 400);
+    }
+
+    $password = Str::random($length);
+    $password = substr(str_shuffle($characters), 0, $length);
+
+    return response()->json(['password' => $password]);
 });
