4578343d093ef9741d41f11e3cf6b2b8e4c3c039
Taran C <t.chana.21.101@gmail.com>
2024-12-10 23:47:09 +0000

    added better styling to the checkout page.

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -15,27 +15,27 @@
                     @csrf
                     <div>
                         <label for="street_address" class="block text-sm font-medium text-gray-700">Street Address</label>
-                        <input type="text" name="street_address" id="street_address" required class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                        <input type="text" name="street_address" id="street_address" required class="mt-1 block w-full p-3 border border-black rounded-md shadow-sm focus:ring-amber focus:border-amber sm:text-sm"  />
                     </div>
                     <div>
                         <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                        <input type="text" name="city" id="city" required class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                        <input type="text" name="city" id="city" required class="mt-1 block w-full p-3 border border-black rounded-md shadow-sm focus:ring-amber focus:border-amber sm:text-sm"  />
                     </div>
                     <div>
                         <label for="county" class="block text-sm font-medium text-gray-700">County</label>
-                        <input type="text" name="county" id="county" required class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                        <input type="text" name="county" id="county" required class="mt-1 block w-full p-3 border border-black rounded-md shadow-sm focus:ring-amber focus:border-amber sm:text-sm"  />
                     </div>
                     <div>
                         <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
-                        <input type="text" name="country" id="country" required class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                        <input type="text" name="country" id="country" required class="mt-1 block w-full p-3 border border-black rounded-md shadow-sm focus:ring-amber focus:border-amber sm:text-sm"  />
                     </div>
                     <div>
                         <label for="post_code" class="block text-sm font-medium text-gray-700">Post Code</label>
-                        <input type="text" name="post_code" id="post_code" required class="mt-1 block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                        <input type="text" name="post_code" id="post_code" required class="mt-1 block w-full p-3 border border-black rounded-md shadow-sm focus:ring-amber focus:border-amber sm:text-sm" />
                     </div>
                     <input type="hidden" name="type" value="shipping" />
                     <div>
-                        <button type="submit" class="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Go To Confirmation</button>
+                        <button type="submit" class="w-full bg-amber text-white py-2 px-4 rounded-md hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber">Go To Confirmation</button>
                     </div>
                 </form>
             </div>
