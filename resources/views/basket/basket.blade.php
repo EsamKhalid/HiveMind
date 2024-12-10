a6f56e399459db28cafcd9bbd476500e63f15e8e
Taran C <t.chana.21.101@gmail.com>
2024-12-10 21:01:24 +0000

    updated basket blade php in order to keep functionality and remove duplicated code

--- resources/views/basket/basket.blade.php
+++ resources/views/basket/basket.blade.php
@@ -3,85 +3,37 @@
 <head>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="flex flex-col min-h-screen">
     <header></header>
     @include('layouts.navbar')
-    <main>
+    <main class="flex-grow">
         <div class="flex justify-center m-4">
             <div class="inline-block">
                 <p class="text-6xl">Basket</p>
                 @if(count($basketItems) === 0)
-    <head>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <header></header>
-        @include('layouts.navbar')
-        <main>
-            <div class="flex justify-center m-4">
-                <div class="inline-block">
-                    <p class="text-6xl">Basket</p>
-                    @if(count($basketItems) === 0)
-                    <p>no items in basket</p>
+                    <p>No items in basket</p>
                 @else
-                    @else
                     <div class="inline-block">
                         @foreach($basketItems as $basketItem)
-                            <div class="bg-amber">
+                            <div class="bg-amber p-4 mb-4 rounded">
                                 <p>{{ $basketItem->product_name }}</p>
                                 <p>Price: {{$basketItem->price}}</p>
-                                <p>quantity: {{$basketItem->quantity}}</p>
-                                <form action="{{ route('basket.updateQuantity') }}" method="post">
+                                <p>Quantity: {{$basketItem->quantity}}</p>
+                                <form action="{{ route('basket.updateQuantity') }}" method="post" class="mb-2">
                                     @csrf
-                                    <input type="text" name="quantity" required />
+                                    <input type="number" name="quantity" required class="border p-1 rounded" />
                                     <input type="hidden" name="product_id" value="{{ $basketItem->product_id }}" />
-                                    <br />
-                                    <button type="submit">Update quantity</button>
-                                    <br />
+                                    <button type="submit" class="bg-blue-500 text-white px-2 py-1 rounded">Update Quantity</button>
                                 </form>
                                 <form action="{{ route('basket.remove') }}" method="post">
                                     @csrf
                                     <input type="hidden" name="product_id" value="{{ $basketItem->product_id }}" />
-                                    <button type="submit">remove</button>
+                                    <button type="submit" class="bg-red-500 text-white px-2 py-1 rounded">Remove</button>
                                 </form>
                             </div>
-                            <br />
-                        <div class="bg-amber">
-                            <p>{{ $basketItem->product_name }}</p>
-                            <p>Price: {{$basketItem->price}}</p>
-                            <p>quantity: {{$basketItem->quantity}}</p>
-                            <form
-                                action="{{ route('basket.updateQuantity') }}"
-                                method="post"
-                            >
-                                @csrf
-                                <input type="text" name="quantity" required />
-                                <input
-                                    type="hidden"
-                                    name="product_id"
-                                    value="{{ $basketItem->product_id }}"
-                                />
-                                <br />
-                                <button type="submit">Update quantity</button>
-                                <br />
-                            </form>
-                            <form
-                                action="{{ route('basket.remove') }}"
-                                method="post"
-                            >
-                                @csrf
-                                <input
-                                    type="hidden"
-                                    name="product_id"
-                                    value="{{ $basketItem->product_id }}"
-                                />
-                                <button type="submit">remove</button>
-                            </form>
-                        </div>
-                        <br />
-                        @endforeach @endif
+                        @endforeach
                     </div>
-                </div>
+                @endif
             </div>
         </div>
     </main>
