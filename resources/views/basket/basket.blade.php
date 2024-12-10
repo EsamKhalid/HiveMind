1d25e6ea9b17a2e0b42c784c9e088640373fd7d0
Bxddo <46075760+Bxddo@users.noreply.github.com>
2024-12-10 19:35:59 +0000

    Basket now deletes on checkout (with @Basanta-Kandel )

--- resources/views/basket/basket.blade.php
+++ resources/views/basket/basket.blade.php
@@ -26,7 +26,7 @@
                                     <span class="item-img-text-link text-wrap ml-3 max-w-[50%] my-auto mx-[10%]">
                                         <p class="text-2xl font-bold leading-6 w-[100%]">{{ $basketItem->product_name }}</p>
                                         <p class="text-2xl leading-8 w-[100%]">Price: {{$basketItem->price}}</p>
-                                        <p class="text-2xl leading-8 w-[100%]">quantity: {{$basketItem->quantity}}</p>
+                                        <p class="text-2xl leading-8 w-[100%]">Quantity: {{$basketItem->quantity}}</p>
                                     </span>
 
 
@@ -47,17 +47,27 @@
                                         <input type="hidden" name="product_id" value="{{ $basketItem->product_id }}" />
                                         <button type="submit">remove</button>
                                     </form>
+
+
                                 </div>
                             </li>
 
                         @endforeach
                 @endif
-                <div class="flex justify-end">
-                    <button type="Checkout"
-                        class=" py-2 a px-4 bg-white text-grey-800 rounded-lg shadow-md hover:text-amber focus:outline-none focus:ring-2 focus:ring-blue-500">
-                        @csrf
-                        <a href="{{ route('checkout.view') }}">Checkout</a>
-                    </button>
+                    <div class="flex justify-end">
+
+                        <h2> Total Price: {{$basket->total_amount}} </h2>
+                        <a></a>
+
+                        @if(count($basketItems) === 0)
+                            <p></p>
+                        @else
+                            <button type="Checkout"
+                                class=" py-2 a px-4 bg-white text-grey-800 rounded-lg shadow-md hover:text-amber focus:outline-none focus:ring-2 focus:ring-blue-500">
+                                @csrf
+                                <a href="{{ route('checkout.view') }}">Checkout</a>
+                            </button>
+                        @endif
                     </div>
             </div>
 
