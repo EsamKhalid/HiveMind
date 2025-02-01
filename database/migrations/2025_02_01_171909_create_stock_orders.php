d27de621388c2f5cbf8983194f707c44a69578c5
Bxddo <46075760+Bxddo@users.noreply.github.com>
2025-02-01 17:27:46 +0000

    Added Stock & Stock_Orders table. Pair programming with @Basanta-Kandel

---
+++ database/migrations/2025_02_01_171909_create_stock_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('stock_id')->constrained('stock')->onDelete('cascade');
+            $table->foreignId('supplier_id')->constrained('suppliers')->onDelete('cascade');
+            $table->integer('stock_quantity');
+            $table->date('lead_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_orders');
+    }
+};
+
+
