459d43204afb7774406fd7e2dd6ff2c270574ae8
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-02-01 18:21:18 +0000

    added flex to center inventory boxes
    once again, the comments speak for themselves

--- resources/views/inventory/inventory.blade.php
+++ resources/views/inventory/inventory.blade.php
@@ -5,9 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
 </head>
 <body>
-    @include('layouts.navbar')
+    @include('layouts.inventory')
+
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="text-center">
+            <h2 class="text-2xl font-bold text-gray-800 mb-4">Inventory</h2>
+            <div class="grid grid-cols-4 gap-10 bg-gray-200 p-6 rounded-lg shadow-md max-w-fit mx-auto" id="INVENTORY">
+            </div>
+        </div>
+    </div>
+
+    <script>
+        
+        // THE FOLLOWING WOULD DO EXACTLY AS IT WOULD IN HTML
+        // THOUGH, I THOUGHT IT'D BE SIMPLER TO JUST MAKE A SCRIPT FOR IT INSTEAD
+
+        // REMOVING THE REDUNDANT OVER-DECLARATION OF CARDS
+
+        const GRID = document.getElementById('INVENTORY');
+        const ITEMS = "w-24 h-24 bg-yellow-200 rounded-lg flex items-center justify-center shadow hover:bg-orange-400 transition-colors duration-300";
+        const ICONS = "fas fa-box text-white-100 text-2xl";
+        
+        for (let INDEX = 0; INDEX < 8; INDEX++) 
+        {
+            GRID.innerHTML += 
+            `
+                <div class="${ITEMS}">
+                    <i class="${ICONS}"></i>
+                </div>
+            `;
+        }
+    </script>
 </body>
 </html>
