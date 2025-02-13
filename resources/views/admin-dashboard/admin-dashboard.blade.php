2b71052deb84f6db4df9e119ace98800c10945a1
Musa Ahmed <154725338+MusaUsingGit@users.noreply.github.com>
2025-02-13 03:30:09 +0000

    some sidebar improvements
    fixed spacing and colouring - and implemented means to route to admin page on admin login

--- resources/views/admin-dashboard/admin-dashboard.blade.php
+++ resources/views/admin-dashboard/admin-dashboard.blade.php
@@ -6,28 +6,23 @@
     <title>Dashboard</title>
 </head>
 
-<body class="bg-white dark:bg-black">
+<body class="bg-white dark:bg-stone-950 flex">
 @include('layouts.sidebar')
-
-    <div id="card" class="m-auto min-w-[50%] bg-yellow-500 dark:bg-gray-400 flex-shrink-0">
-            <div class="text-7xl">
-            Hello, Admin User
+    <div id="notifications" class=" mx-auto mb-auto flex-col w-[80%] mt-[4%]">
+        <p class="text-7xl text-white p-5 bg-yellow-400 dark:bg-gray-400 dark:bg-opacity-40 rounded-md"> <i class="fa-solid fa-inbox text-7xl mr-4 my-auto"></i>NOTIFICATIONS</p>
+    <div class="bg-yellow-100 dark:bg-red-100 border-t-4 border-yellow-800 dark:border-red-700 rounded-b text-yellow-900 dark:text-red-900 px-4 py-3 shadow-md my-5 mx-5">
+            <div class="flex">
+            <i class="fa-solid fa-warehouse text-2xl mr-4 my-auto "></i>
+                <div>
+                    <p class="font-bold">An Update has been made to the inventory</p>
+                    <p class="text-sm">New shipment for product x has been made</p>
+                </div>
             </div>
         </div>
+       
+        
+    </div>
 </body>
-<script>
-    var checkbox = document.getElementById("drawer-toggle");
-    checkbox.addEventListener("change", function () {
-        if (this.checked) {
-            document.getElementById("hamburger").classList.add("hidden");
-            document.getElementById("cross").classList.remove("hidden");
-        } else {
-            document.getElementById("cross").classList.add("hidden");
-            document.getElementById("hamburger").classList.remove("hidden");
-        }
-    });
 
-
-</script>
 
 </html>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,14 @@
 
-<div id="sidebar" class="flex w-[0%] h-[100%] m-auto">
+<div id="sidebar" class="flex w-[0%] h-[100%]">
         <div class="flex ">
-            <input type="checkbox" id="drawer-toggle" class="relative sr-only peer opacity-0" checked>
+            <input type="checkbox" id="drawer-toggle" class="relative sr-only peer opacity-0">
             <label for="drawer-toggle"
                 class="z-10 flex flex-col absolute top-0 left-0 p-4 transition-all ease-in-out duration-1000 bg-cybpnk-ylw2 rounded-lg peer-checked:left-[80%] peer-checked:rotate-[720deg] md:peer-checked:left-[35%] lg:peer-checked:left-[400px] ">
                 <i id="hamburger" class="fa-solid fa-bars"></i>
                 <i id="cross" class="fa-solid fa-x hidden"></i>
             </label>
             <div
-                class="fixed top-0 left-0 z-1 w-[80%] lg:w-[400px] m:w-[35%] h-full transition-all ease-in-out duration-1000 transform -translate-x-full bg-white dark:bg-black shadow-lg dark:shadow-md dark:shadow-gray-300 peer-checked:translate-x-0">
+                class="fixed top-0 left-0 z-1 w-[80%] lg:w-[400px] m:w-[35%] h-full transition-all ease-in-out duration-1000 transform -translate-x-full bg-white dark:bg-stone-950 shadow-lg dark:shadow-md dark:shadow-gray-300 peer-checked:translate-x-0">
                 <div class="px-6 py-4 h-full ">
                     <div class="flex justify-start align-middle mb-10">
                         <img src="{{ asset('../Images/HiveMind dark Logo.png') }}" alt="HiveMind Logo"
@@ -19,23 +19,16 @@
                     <hr class="solid border-black dark:border-yellow-300">
                     <div class="flex flex-col justify-between  h-full">
                         <div id="top-half-sidebar" class="flex flex-col dark:text-white">
-                        <a class="text-3xl my-4 p-2 w-full text-nowrap">Notifications<i class="fa-solid fa-inbox ml-5 mr-1 text-yellow-500"></i></a>
-                            <a class="text-3xl my-4 p-2 w-full text-nowrap">Inventory <i
-                                    class="fa-solid fa-warehouse ml-5 mr-1 text-yellow-500"></i></a>
-                                    
-                            <a class="text-3xl my-4 p-2 w-full text-nowrap">Manage users <i
-                                    class="fa-solid fa-sitemap ml-5 mr-1 text-yellow-500"></i></a>
-                            <a class="text-3xl my-4 p-2 w-full text-nowrap">View Statistics <i
-                                    class="fa-solid fa-chart-line ml-5 mr-1 text-yellow-500"></i></a>
-                            <a class="text-3xl my-4 p-2 w-full text-nowrap">Customer View <i
-                                    class="fa-solid fa-right-left ml-5 mr-1 text-yellow-500"></i></a>
-
+                            <a class="text-3xl my-4 p-3 w-full text-nowrap dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{ route('admin-dashboard') }}><p>Notifications</p><i class="fa-solid fa-inbox text-yellow-500 text-4xl"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full text-nowrap dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('admin-dashboard' /*inventory*/)}}><p>Inventory</p> <i class="fa-solid fa-warehouse text-yellow-500 text-4xl"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full text-nowrap dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('admin-dashboard' /*User MNG Page*/)}}><p>Manage users</p> <i class="fa-solid fa-sitemap text-yellow-500 text-4xl"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full text-nowrap dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('admin-dashboard' /*STATS*/)}}><p>View Statistics</p> <i class="fa-solid fa-chart-line text-yellow-500 text-4xl"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full text-nowrap dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('home' /*Customer View*/)}}><p>Customer View</p> <i class="fa-solid fa-right-left text-yellow-500 text-4xl"></i></a>
                         </div>
                         <div id="bottom-half-sidebar" class="flex flex-col mb-[30%] dark:text-white">
-                            <a class="text-3xl my-4 p-2 w-full">Account <i class="fa-solid fa-user ml-5 mr-1 text-yellow-500"></i></a>
-                            <a class="text-3xl my-4 p-2 w-full">Settings<i class="fa-solid fa-gear ml-5 mr-1 text-yellow-500"></i></a>
-                            <a class="text-3xl my-4 p-2 w-full">Help <i
-                                    class="fa-solid fa-circle-question ml-5 mr-1 text-yellow-500"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('account' /*Customer View*/)}}><p>Account</p> <i class="fa-solid fa-user ml-5 mr-1 text-yellow-500"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('home' /*Customer View*/)}}><p>Settings</p> <i class="fa-solid fa-gear ml-5 mr-1 text-yellow-500"></i></a>
+                            <a class="text-3xl my-4 p-2 w-full dark:hover:bg-stone-800 hover:bg-yellow-200 justify-between flex" href={{route('home' /*Customer View*/)}}><p>Help</p><i class="fa-solid fa-circle-question ml-5 mr-1 text-yellow-500"></i></a>
                         </div>
                     </div>
                 </div>
