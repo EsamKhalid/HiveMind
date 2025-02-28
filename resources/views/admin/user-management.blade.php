f82ca33785045ae455793d668598dc9eea98be5c
Esam Khalid <esam-khalid@outlook.com>
2025-02-28 15:56:45 +0000

    listing more attributes

--- resources/views/admin/user-management.blade.php
+++ resources/views/admin/user-management.blade.php
@@ -9,8 +9,31 @@
         <!-- @include('layouts.sidebar') -->
 
         <h1>User Management</h1>
-        @foreach($users as $user)
-        <p>{{$user->first_name}}</p>
-        @endforeach
+        <div class="flex justify-center">
+            <div class="">
+                @foreach($users as $user)
+                <div
+                    class="bg-white mx-2 rounded flex justify-between text-center items-center p-2 [&_*]:pr-3 mb-2 border"
+                >
+                    <p class="text-2xl text-center h-fit">
+                        {{$user->first_name}}
+                    </p>
+                    <p class="text-2xl text-center h-fit">
+                        {{$user->last_name}}
+                    </p>
+                    <p class="text-2xl text-center h-fit">
+                        {{$user->phone_number}}
+                    </p>
+                    <p class="text-2xl text-center h-fit">
+                        {{$user->email_address}}
+                    </p>
+                    <p class="text-2xl text-center h-fit">
+                        {{$user->permission_level}}
+                    </p>
+                </div>
+
+                @endforeach
+            </div>
+        </div>
     </body>
-</html
+</html>
