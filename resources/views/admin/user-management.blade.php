90f4d172b318161fd9d6fc78029588e52cfdd2ea
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-03-01 19:48:29 +0000

    Changes to the table

--- resources/views/admin/user-management.blade.php
+++ resources/views/admin/user-management.blade.php
@@ -6,33 +6,42 @@
     </head>
 
     <body class="bg-white dark:bg-stone-950 flex">
-        <!-- @include('layouts.sidebar') -->
+        @include('layouts.sidebar')
 
-        <h1>User Management</h1>
         <div class="flex justify-center">
-            <div class="">
-                @foreach($users as $user)
-                <div
-                    class="bg-white mx-2 rounded flex justify-between text-center items-center p-2 [&_*]:pr-3 mb-2 border"
-                >
-                    <p class="text-2xl text-center h-fit">
-                        {{$user->first_name}}
-                    </p>
-                    <p class="text-2xl text-center h-fit">
-                        {{$user->last_name}}
-                    </p>
-                    <p class="text-2xl text-center h-fit">
-                        {{$user->phone_number}}
-                    </p>
-                    <p class="text-2xl text-center h-fit">
-                        {{$user->email_address}}
-                    </p>
-                    <p class="text-2xl text-center h-fit">
-                        {{$user->permission_level}}
-                    </p>
-                </div>
-
-                @endforeach
+            <div class="mt-10">
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr
+                            class="bg-white mx-2 rounded text-center items-center p-2 mb-2 border text-2xl"
+                        >
+                            <th class="px-4 py-2">First Name</th>
+                            <th class="px-4 py-2">Last Name</th>
+                            <th class="px-4 py-2">Email Address</th>
+                            <th class="px-4 py-2">Permission Level</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr
+                            class="bg-white mx-2 rounded text-center items-center p-2 mb-2 border"
+                        >
+                            <td class="px-4 py-2 text-xl">
+                                {{$user->first_name}}
+                            </td>
+                            <td class="px-4 py-2 text-2xl">
+                                {{$user->last_name}}
+                            </td>
+                            <td class="px-4 py-2 text-2xl">
+                                {{$user->email_address}}
+                            </td>
+                            <td class="px-4 py-2 text-2xl">
+                                {{$user->permission_level}}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </body>
