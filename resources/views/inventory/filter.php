9fcb83b19e4b8951b302ded81be276ee6d2d20c7
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-02 19:15:04 +0000

    added tenary operator

--- resources/views/inventory/filter.php
+++ resources/views/inventory/filter.php
@@ -1,5 +1,10 @@
 <?php
 
+// THE FOLLOWING FILE IS JUST A GENERAL REWORK OF THE JS LOGIC IMPLEMENTED IN THE PROVIDED
+// VIEW FILE FOR THE INVENTORY SYSTEM
+
+// MADE AT THE REQUEST OF ESAM TO PROVIDE SCALABILITY IN BEING ABLE TO REFACTOR ACCORDINGLY
+
 $TYPE_COLOURS = 
 [
     'BEAUTY' => 'bg-pink-100',
@@ -21,5 +26,20 @@ $TYPE_ICONS =
     'MERCH' => 'fa-tshirt',
     'HOME' => 'fa-home'
 ];
+
+function GENERATE_CARD_HTML($PRODUCT, $TYPE_ICONS)
+{
+    $stock_level = $PRODUCT['stock_level'];
+
+    [$STATUS, $COLOUR] = $stock_level === 0 
+    ? ['Out of Stock', 'text-red-400']
+    : ($stock_level < 6
+        ? ['Extremely Low', 'text-red-500']
+        : ($stock_level < 20
+            ? ['Running Low', 'text-amber-600']
+            : ['Sufficient Stock', 'text-green-600']
+        )
+    );
+}
 
 ?>
