c3843394a9be14d2f5a3868552f996301ae650ba
Aryanjot Grewal <aryanzgrewal@gmail.com>
2025-03-02 13:29:47 +0000

    Added success/error feedback
    User now receives success or error feedback.

--- resources/views/orders/orders.blade.php
+++ resources/views/orders/orders.blade.php
@@ -22,6 +22,17 @@
             </div>
         </header>
 
+        @if (session('success'))
+            <div class="bg-green-100 text-green-800 p-4 rounded mb-4">
+                {{ session('success') }}
+            </div>
+        @endif
+        @if (session('error'))
+            <div class="bg-red-100 text-red-800 p-4 rounded mb-4">
+                  {{ session('error') }}
+            </div>
+        @endif
+
         <main>
             <section class="max-w-7xl mx-auto p-6">
                 @if ($orders->isEmpty())
