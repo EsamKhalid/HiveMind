162ce6e48583516a5b5b4df2c73e9535a4e77a36
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-02 19:04:57 +0000

    WIP filtering re-tool
    putting this in /views/inventory for now

---
+++ resources/views/inventory/filter.php
@@ -0,0 +1,25 @@
+<?php
+
+$TYPE_COLOURS = 
+[
+    'BEAUTY' => 'bg-pink-100',
+    'HEALTH' => 'bg-green-100',
+    'HAIRCARE' => 'bg-purple-100',
+    'SKINCARE' => 'bg-yellow-100',
+    'BODYCARE' => 'bg-red-100',
+    'MERCH' => 'bg-blue-100',
+    'HOME' => 'bg-indigo-100'
+];
+
+$TYPE_ICONS = 
+[
+    'BEAUTY' => 'fa-spa',
+    'HEALTH' => 'fa-heartbeat',
+    'HAIRCARE' => 'fa-air-freshener',
+    'SKINCARE' => 'fa-pump-soap',
+    'BODYCARE' => 'fa-shower',
+    'MERCH' => 'fa-tshirt',
+    'HOME' => 'fa-home'
+];
+
+?>
