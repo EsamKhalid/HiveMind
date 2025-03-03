8a42b35c0b41b702a0833f0e5f3b69cdc7fc6e4d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-03 20:14:48 +0000

    explicitly defined table name and return basis

--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -7,8 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
+    use HasFactory;
+
+    protected $table = 'reviews';
+
+    protected $fillable = 
+    [
+        'product_id',
+        'user_id',
+        'rating',
+        'comment',
+    ];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
