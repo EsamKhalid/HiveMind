0499c4c3dc43accb408f82f9648cc386f3129bc3
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-03 20:58:41 +0000

    added wip routing for reviews

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DetailsController;
 
 use App\Http\Controllers\BasketController;
+use App\Http\Controllers\InventoryController;
 
-use App\Http\Controllers\AdminController;
 Route::get('/', function () {
     return view('home');
 });
@@ -64,17 +64,7 @@ Route::post('signup.signup', [SignupController::class, 'store'])->name('signup.s
 
 Route::get('products',[ProductController::class,'list'])->name('products');
 
-
-
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-
-
-
-
-
-
-
-/**Middleware is a mechanism that allows you to perform actions such as authentication, logging, validaition and such 
+/**Middleware is a mehcnism that allows you to perform actions such as authentication, logging, validaition and such 
  * before or after the request is processed by your controller. Ultimately, it acts as a bridge between request and a response. */
 
 
@@ -103,14 +93,16 @@ Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('adm
     Route::get('contact', [ContactController::class, 'view'])->name('contact');
     Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
 
-    // Routes for details page - Aryan
+    Route::get('admin/inventory', [InventoryController::class, 'view'])->name('admin.inventory');
     Route::get('details', [DetailsController::class, 'view'])->name('user.details');
-    Route::post('details/update', [DetailsController::class, 'update'])->name('user.details.update');
-    
-    // Routes for return order functionality - Aryan
-    Route::get('/orders/{id}/return', [OrderController::class, 'showReturnForm'])->name('orders.return');
-    Route::post('/orders/{id}/return', [OrderController::class, 'submitReturnRequest'])->name('orders.return.submit');
-
-
     Route::get('terms', [UserController::class, 'terms'])->name('user.terms');
     Route::get('settings', [UserController::class, 'settings'])->name('user.settings');
+    
+    // NOTE FROM HARRY (15/02/25)
+    // IF YOU WANT TO USE MY INVENTORY CODE FROM "resources/views/inventory/inventory.blade.php"
+    // YOU MIGHT HAVE TO UPDATE THIS ROUTE GET ACCORDINGLY ASSUMING YOU ARE RUNNING FROM "resources/views/admin/inventory.blade.php"
+
+    Route::get('/inventory', [ProductController::class, 'inventory_products']);
+    Route::get('admin', [AdminController::class, 'adm'])->name('adm');
+
+    Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
