d73d660271446607e0c600b1abd1d2d567c2a826
Muneeb <230072361@aston.ac.uk>
2025-03-03 22:55:27 +0000

    Updated Stock Order Migration

--- database/migrations/2025_02_01_171909_create_stock_orders.php
+++ database/migrations/2025_02_01_171909_create_stock_orders.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->foreignId('supplier_id')->constrained('suppliers')->onDelete('cascade');
             $table->integer('stock_quantity');
-            $table->date('lead_time');
+            $table->integer('lead_time');
             $table->date('order_date');
             $table->timestamps();
         });
