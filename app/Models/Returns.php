92261edc4d56690a152a5dc742829094413ebdd3
Muneeb <230072361@aston.ac.uk>
2025-03-04 12:42:55 +0000

    Added Models for Returns & ReturnItem. (Model for "Return" is "Returns" as "Return" is reserved by PHP.)

---
+++ app/Models/Returns.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Returns extends Model
+{
+    protected $table = 'returns';
+
+    protected $fillable = [
+        'order_id',
+        'return_status',
+        'return_date',
+        'comments',
+    ];
+}
