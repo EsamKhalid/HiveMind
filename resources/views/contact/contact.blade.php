d6ca9bf249f02090b9a5fc906517cb351581bb44
Taran C <t.chana.21.101@gmail.com>
2025-03-07 15:46:34 +0000

    change products displayed so that existing products are displayed on the products page, change the duration of the transition on home, updated dark mode (change by @Musa) on contact page, update styling on faq, changed navbar duration + transition changed

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -6,11 +6,11 @@
         <title>Contact Us</title>
     </head>
 
-    <body>
+    <body class="dark:bg-stone-900">
     
         @include('layouts.navbar')
         
-        <main class="text-center">
+        <main>
             @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -21,52 +21,50 @@
         </div>
         @endif
         <!-- title -->
-        <section class="bg-yellow-50 text-center py-16">
-            <h1 class="text-4xl font-bold text-orange-950">Get in Touch</h1>
-            <p class="text-lg text-gray-600 mt-4"> 
+        <section class="bg-yellow-50  dark:dark:bg-stone-700 text-center py-16 transition-all duration-1000">
+            <h1 class="text-4xl font-bold text-orange-950 dark:text-amber">Get in Touch</h1>
+            <p class="text-lg text-gray-600 dark:text-white mt-4"> 
                 Want to get in touch? We'd be buzzing (get it?) to hear from you. Here's how you can reach us...
             </p>
         </section>
 
             <!-- contact details -->
-                <section class="contact-options h-3/4 grid grid-cols-1 md:grid-cols-2 gap-6 p-8 bg-white">
+                <section class="contact-options h-3/4 grid grid-cols-1 md:grid-cols-2 gap-6 p-8 bg-white dark:bg-stone-900 transition-all duration-1000">
                     <!-- card 1: contact options -->
-                    <div class="card bg-yellow-50 p-6 rounded-lg shadow-md text-center">
-                        <div class="icon text-3xl text-blue-600 mb-4">📞</div>
-                        <h2 class="text-xl font-bold mb-2">Contact Us</h2>
-                        <p class="text-gray-600 mb-4">Contact us either via phone or email address.</p>
-                        <a href="+353-123-4567" class="text-orange-900">+353-123-4567</a><br>
-                        <a href="mailto:hivemind.business@outlook.com" class="text-orange-900">admin@hivemind.com</a><br><br>
-                        <p class="text-gray-600 mb-4">Find us at our bee-quarters at:</p>
-                        <p class="text-gray-950">HiveMind</p>
-                        <p class="text-gray-950">123 Beehive Lane</p>
-                        <p class="text-gray-950">Dublin, Ireland</p>
-                        <br/>   
-                        <p class="text-gray-950">Check out our <a href="{{ route('faq') }}" class="text-lg text-blue-500 hover:underline">FAQs</a> for more information.</p>
+                    <div class="card bg-yellow-50 dark:dark:bg-stone-700 p-6 rounded-lg shadow-md text-center transition-all duration-1000">
+                        <div class="icon text-3xl text-blue-600 mb-4 dark:text-white">📞</div>
+                        <h2 class="text-xl font-bold mb-2 dark:text-amber">Contact Us</h2>
+                        <p class="text-gray-600 mb-4 dark:text-white">Contact us either via phone or email address.</p>
+                        <a href="+353-123-4567" class="text-orange-900 dark:text-orange-300 ">+353-123-4567</a><br>
+                        <a href="mailto:hivemind.business@outlook.com" class="text-orange-900 dark:text-orange-300 ">admin@hivemind.com</a><br><br>
+                        <p class="text-gray-600 mb-4 dark:text-white">Find us at our bee-quarters at:</p>
+                        <p class="text-gray-950 dark:text-orange-300">HiveMind</p>
+                        <p class="text-gray-950 dark:text-orange-300">123 Beehive Lane</p>
+                        <p class="text-gray-950 dark:text-orange-300">Dublin, Ireland</p>
+                        <p class="text-gray-950 dark:text-orange-300">Check out our <a href="{{ route('faq') }}" class="text-lg font-bold text-blue-500 hover:underline dark:text-orange-300">FAQs</a> for more information.</p>
                     </div>
-                    
 
 
                 <!-- card 2: contact form -->
-                <div class="card bg-yellow-50 p-6 rounded-lg shadow-md text-center">
+                <div class="card bg-yellow-50 dark:dark:bg-stone-700 p-6 rounded-lg shadow-md text-center transition-all duration-1000">
                     <div class="icon text-3xl text-green-600 mb-4">✉️</div>
-                    <h2 class="text-xl font-bold mb-2">Online Enquiry</h2>
-                    <p class="text-gray-600 mb-4">Fill out the form below, and we'll get back to you as soon as possible.</p>
+                    <h2 class="text-xl font-bold mb-2 dark:text-amber">Online Enquiry</h2>
+                    <p class="text-gray-600 mb-4 dark:text-white">Fill out the form below, and we'll get back to you as soon as possible.</p>
                         
                     <!-- contact form -->
                     <form action="{{ route('contact.store') }}" method="POST" id="contact-us-form">
                         @csrf
-                        <input type="text" name="name" placeholder="Your Name" class="w-full p-3 mb-4 border border-gray-300 rounded" required>
-                        <input type="email" name="email_address" placeholder="Your Email" class="w-full p-3 mb-4 border border-gray-300 rounded" required>
-                        <textarea name="enquiry" placeholder="Your Message (max 500 characters)" class="w-full p-3 mb-4 border border-gray-300 rounded" rows="4" required></textarea>
-                        <button type="submit" class="bg-yellow-400 text-white py-2 px-6 rounded-md hover:bg-yellow-500">Submit</button>
+                        <input type="text" name="name" placeholder="Your Name" class="w-full p-3 mb-4 border border-gray-300 rounded dark:bg-stone-300" required>
+                        <input type="email" name="email_address" placeholder="Your Email" class="w-full p-3 mb-4 border border-gray-300 rounded dark:bg-stone-300" required>
+                        <textarea name="enquiry" placeholder="Your Message (max 500 characters)" class="w-full p-3 mb-4 border border-gray-300 rounded dark:bg-stone-300" rows="4" required></textarea>
+                        <button type="submit" class="bg-yellow-400 text-white py-2 px-6 rounded-md hover:bg-yellow-50 dark:dark:bg-stone-900 dark:hover:text-amber">Submit</button>
                     </form>
                 </div>
 
             </section>
 
                 <!-- pop-up when user submits an enquiry -->
-                <div class="popup-overlay hidden fixed top-0 left-0 w-full h-full" id="popupOverlay"></div>
+                <div class="popup-overlay hidden fixed top-0 left-0 w-full h-full transition-all duration-1000" id="popupOverlay"></div>
                     <div class="popup hidden fixed top-10 left-1/2 -translate-x-1/2 bg-white p-5 rounded-lg shadow-lg" id="popup">
                         <span class="close absolute top-1 right-2.5 text-xl cursor-pointer text-gray-800" id="closePopup">&times;</span>
                         <div class="popup-content text-center">
