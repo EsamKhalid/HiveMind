b9d8862931e9e59d3bbf12210d7c9f11d75c4421
Muneeb <muneeb.c2201@gmail.com>
2025-03-10 02:27:37 +0000

    Added notifications for new users, user orders, and stock orders. Pair programming with @Basanta-Kandel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,17 +6,44 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
+use App\Models\Users;
+use App\Models\Order;
+use App\Models\StockOrder;
+
 class AdminController extends Controller
 {
-    public function dashboard(){
+   public function dashboard()
+   {
 
-       return view('admin.dashboard');
+      $userCreated = Users::select('id', 'first_name', 'last_name', 'created_at')
+         ->selectRaw('"userCreated" AS type')
+         ->where('created_at', '>=', now()->subDay())
+         ->get();
 
-    }
+      $userOrders = Order::select('id', 'total_amount', 'created_at')
+         ->selectRaw('"userOrder" AS type')
+         ->where('created_at', '>=', now()->subDay())
+         ->get();
 
-    public function notifs(){
+      $stockOrders = StockOrder::select('id', 'product_id', 'stock_quantity', 'created_at')
+         ->selectRaw('"stockOrder" AS type')
+         ->where('created_at', '>=', now()->subDay())
+         ->get();
 
-        return view('admin.notifications');
-    
-     }
+      $notifications = $userCreated->concat($stockOrders)->concat($userOrders)->values()->sortByDesc('created_at');
+
+      return view('admin.dashboard', [
+         'notifications' => $notifications,
+      ]);
+
+      //return view('admin.dashboard');
+
+   }
+
+   public function notifs()
+   {
+
+      return view('admin.notifications');
+
+   }
 }
