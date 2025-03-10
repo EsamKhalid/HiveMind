de93114912a5ad9f61bf8c43c667730069dc7ed7
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-03-10 00:40:59 +0000

    Started to rework product review page and readded deleted css? not sure how it got deleted

--- resources/views/review/productReview.blade.php
+++ resources/views/review/productReview.blade.php
@@ -13,84 +13,95 @@
         </div>
 
         <main class="flex justify-center items-center min-h-screen">
-            <div class="flex justify-center text-black">
-                <p>Review: {{$product->product_name}}</p>
-                <img
-                    class="size-[20%]"
-                    src="{{ asset('Images/product images/' . $product->product_name . '.png') }}"
-                />
-                <form
-                    action="{{ route('review.storeProductReview', $id) }}"
-                    method="POST"
-                    class="space-y-10 h-full"
-                >
-                    @csrf
-                    <div class="rating">
-                        <input
-                            type="radio"
-                            id="star5"
-                            name="rating"
-                            value="5"
-                        />
-                        <label
-                            class="star"
-                            for="star5"
-                            title="Awesome"
-                            aria-hidden="true"
-                        ></label>
-                        <input
-                            type="radio"
-                            id="star4"
-                            name="rating"
-                            value="4"
-                        />
-                        <label
-                            class="star"
-                            for="star4"
-                            title="Great"
-                            aria-hidden="true"
-                        ></label>
-                        <input
-                            type="radio"
-                            id="star3"
-                            name="rating"
-                            value="3"
-                        />
-                        <label
-                            class="star"
-                            for="star3"
-                            title="Very good"
-                            aria-hidden="true"
-                        ></label>
-                        <input
-                            type="radio"
-                            id="star2"
-                            name="rating"
-                            value="2"
-                        />
-                        <label
-                            class="star"
-                            for="star2"
-                            title="Good"
-                            aria-hidden="true"
-                        ></label>
-                        <input
-                            type="radio"
-                            id="star1"
-                            name="rating"
-                            value="1"
-                        />
-                        <label
-                            class="star"
-                            for="star1"
-                            title="Bad"
-                            aria-hidden="true"
-                        ></label>
-                    </div>
-                    <input type="text" name="review" />
-                    <input type="submit" />
-                    <a href="{{ route('orders') }}">Skip</a>
-                </form>
+            <div class="inline-grid border justify-center text-center">
+                <div class="flex justify-center">
+                    <p class="border size-fit text-3xl">
+                        Review: {{$product->product_name}}
+                    </p>
+                </div>
+
+                <div class="flex justify-center">
+                    <img
+                        class="size-[20vw]"
+                        src="{{ asset('Images/product images/' . $product->product_name . '.png') }}"
+                    />
+                </div>
+
+                <div class="flex justify-center">
+                    <form
+                        action="{{ route('review.storeProductReview', $id) }}"
+                        method="POST"
+                        class="h-full size-fit inline-grid justify-center"
+                    >
+                        @csrf
+                        <div class="rating">
+                            <input
+                                type="radio"
+                                id="star5"
+                                name="rating"
+                                value="5"
+                            />
+                            <label
+                                class="star"
+                                for="star5"
+                                title="Awesome"
+                                aria-hidden="true"
+                            ></label>
+                            <input
+                                type="radio"
+                                id="star4"
+                                name="rating"
+                                value="4"
+                            />
+                            <label
+                                class="star"
+                                for="star4"
+                                title="Great"
+                                aria-hidden="true"
+                            ></label>
+                            <input
+                                type="radio"
+                                id="star3"
+                                name="rating"
+                                value="3"
+                            />
+                            <label
+                                class="star"
+                                for="star3"
+                                title="Very good"
+                                aria-hidden="true"
+                            ></label>
+                            <input
+                                type="radio"
+                                id="star2"
+                                name="rating"
+                                value="2"
+                            />
+                            <label
+                                class="star"
+                                for="star2"
+                                title="Good"
+                                aria-hidden="true"
+                            ></label>
+                            <input
+                                type="radio"
+                                id="star1"
+                                name="rating"
+                                value="1"
+                            />
+                            <label
+                                class="star"
+                                for="star1"
+                                title="Bad"
+                                aria-hidden="true"
+                            ></label>
+                        </div>
+                        <label for="review">Review</label>
+                        <input type="text" name="review" />
+                        <input type="submit" class="cursor-pointer" />
+                        <a href="{{ route('orders') }}">Skip</a>
+                    </form>
+                </div>
             </div>
         </main>
 
