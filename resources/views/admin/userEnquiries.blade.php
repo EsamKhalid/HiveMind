de62509cce661def34fe23177ae0ce145653ccc4
Esam Khalid <esam-khalid@outlook.com>
2025-03-11 21:33:03 +0000

    Put icon on left :skull:

--- resources/views/admin/userEnquiries.blade.php
+++ resources/views/admin/userEnquiries.blade.php
@@ -15,9 +15,9 @@
         <main class="flex justify-center items-center text-center min-h-screen">
             <div class="w-full max-w-4xl p-4">
                 <p class="text-7xl text-white p-5 bg-yellow-400 dark:bg-gray-400 dark:bg-opacity-40 rounded-md my-10">
-                    
-                    User Enquiries
                     <i class="fa-solid fa-question-circle text-7xl mr-4 my-auto"></i>
+                    User Enquiries
+                    
                 </p>  
                 <form
                     action="{{ route('admin.userEnquiries') }}"
