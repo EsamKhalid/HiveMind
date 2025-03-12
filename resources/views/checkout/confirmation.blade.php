dfe15c13ef36120fd5a70cf08da9ae2a7037afb4
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-03-12 07:54:02 +0000

    Now routes differently based on if you are logged in or not

--- resources/views/checkout/confirmation.blade.php
+++ resources/views/checkout/confirmation.blade.php
@@ -13,12 +13,19 @@
                 <div class="inline-block">
                     <p class="text-6xl">Confirmation</p>
 
+                    @auth
                     <a
                         href="{{ route('orders') }}"
                         class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4"
                         >Click here to view your order</a
                     >
-                    {{ $confirmation_number }}
+                    @else
+                    <a
+                        href="{{ route('orders.guest.validate') }}"
+                        class="bg-ghost-white rounded-lg text-4xl p-1 h-fit w-full m-4"
+                        >Click here to view your order</a
+                    >
+                    @endauth
                 </div>
             </div>
         </main>
