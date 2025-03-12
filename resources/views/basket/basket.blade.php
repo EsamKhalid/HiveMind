19ea1ec7217db2e2cfd093e6e7dca606e801aa62
Musa Ahmed <154725338+MusaUsingGit@users.noreply.github.com>
2025-03-12 23:54:58 +0000

    no scrollbar css added basket last improvement

--- resources/views/basket/basket.blade.php
+++ resources/views/basket/basket.blade.php
@@ -20,7 +20,7 @@
                 }
             }
         }
-        function hideCheckoutButton(){
+        function hideCheckoutButton() {
             var elements = document.getElementsByClassName('mobileViewAddressLine');
             for (let element of elements) {
                 if (!element.classList.contains('hidden')) {
@@ -30,7 +30,7 @@
                 }
             }
         }
-        
+
     </script>
 </head>
 
@@ -39,7 +39,8 @@
 
     <main>
         <div class="flex justify-center m-4">
-            <div class="card bg-yellow-50 dark:bg-stone-700 p-8 rounded-lg shadow-md text-center w-[80%] transition-colors duration-1000">
+            <div
+                class="card bg-yellow-50 dark:bg-stone-700 p-8 rounded-lg text-center w-[80%] transition-colors duration-1000">
                 <h1 class="text-6xl mb-6 dark:text-amber transition-colors duration-1000">Basket</h1>
 
                 @if($errors->any())
@@ -48,107 +49,111 @@
 
                 <!--Main content container-->
                 <div class="flex justify-between transition-all duration-1000 flex-col-reverse xl:flex-row">
-                    <!--Address Form-->
-                    <div class="bg-yellow-300 dark:bg-amber p-5 rounded-lg w-full xl:w-1/4 hidden xl:flex xl:flex-col justify-between">
+                    <!--Address Form container-->
+                    <div
+                        class="bg-white dark:bg-yellow-500 shadow-md p-5 rounded-lg w-full xl:w-1/4 hidden xl:flex xl:flex-col justify-between transition-colors duration-1000">
                         @include('layouts.addressForm')
                         <!--Address Form Total+Checkout Button-->
 
                     </div>
                     @if(count($basketItems) > 0)
-                    <div class="xl:hidden justify-center w-[80%] rounded-lg mx-auto fixed left-0 right-0 bottom-0 bg-transparent z-10 h-fit mt-[10%] -mb-3">
-                    <input type="checkbox" id="drawer-toggle" class="relative sr-only peer opacity-0"
-                            onclick="hideCheckoutButton()" />
-                        <label for="drawer-toggle"
-                            class="z-10 flex flex-col absolute top-0 right-0 left-0 p-4 transition-all ease-in-out bg-amber dark:bg-stone-90 rounded-lg peer-checked:top-[80%]">
-                            <i class="fa-solid fa-chevron-up"></i>
-                        </label>
-
-                    <div id="checkoutbuttonbox"
-                        class="pt-6 max-h-[80%] flex-col xl:hidden items-center bg-yellow-200 dark:bg-stone-800 peer-checked:peer-checked:translate-y-[100%] transition-all ease-in-out">
-                        
                         <div
-                            class="mobileViewAddressLine hidden xl:hidden justify-center mt-3 transition-all ease-in-out ">
-                        </div>
-                       
-                        <h3 class=" text-black dark:text-white ">Delivery Address:
-                            <p class="text-black dark:text-white justify-center flex transition-colors duration-1000">{{$address->street_address}}</p>
-                        </h3>
-                        <div class=" mobileViewAddressLine hidden flex-col xl:hidden">
+                            class="xl:hidden justify-center w-[80%] rounded-lg mx-auto fixed left-0 right-0 bottom-0 bg-transparent z-10 h-fit mt-[10%] -mb-3 ">
+                            <input type="checkbox" id="drawer-toggle" class="relative sr-only peer opacity-0"
+                                onclick="hideCheckoutButton()" />
+                            <label for="drawer-toggle"
+                                class="z-10 flex flex-col absolute top-0 right-0 left-0 p-4 transition-all ease-in-out bg-amber dark:bg-stone-90 rounded-lg peer-checked:top-[80%]">
+                                <i class="fa-solid fa-chevron-up"></i>
+                            </label>
 
-                        @include('layouts.addressForm')
+                            <div id="checkoutbuttonbox"
+                                class="pt-6 max-h-[80%] flex-col xl:hidden items-center bg-yellow-200 dark:bg-stone-800 peer-checked:peer-checked:translate-y-[100%] transition-all ease-in-out">
 
-                        </div>
-                        <div class="flex justify-center">
-                            <a onclick="toggleMobileAddressBox()" id="show-hide"
-                                class="text-black bg-stone-500 hover:text-white cursor-pointer p-1 rounded-md m-2 transition-colors duration-1000">Show
-                                Details</a>
-                        </div>
+                                <div
+                                    class="mobileViewAddressLine hidden xl:hidden justify-center mt-3 transition-all ease-in-out ">
+                                </div>
 
-                        <h2
-                            class="text-xl text-center mb-2 p-2 mt-3 mx-auto bg-white text-grey-800 rounded shadow-md w-1/2">
-                            Subtotal: £{{$basket->total_amount}}
-                        </h2>
-                        <div class="flex justify-center mt-auto w-full">
-                            <button type="Checkout"
-                                class="bg-amber text-black hover:text-white my-3 py-2 px-8 rounded-lg shadow-lg hover:underlinehover:text-white w-[80%] font-bold transition-colors duration-1000">
-                                @csrf
-                                <a href="{{ route('checkout.checkout') }}">Checkout</a>
-                            </button>
+                                <h3 class=" text-black dark:text-white ">Delivery Address:
+                                    <p
+                                        class="text-black dark:text-white justify-center flex transition-colors duration-1000">
+                                        {{$address->street_address}}</p>
+                                </h3>
+                                <div class=" mobileViewAddressLine hidden flex-col xl:hidden">
+
+                                    @include('layouts.addressForm')
+
+                                </div>
+                                <div class="flex justify-center">
+                                    <a onclick="toggleMobileAddressBox()" id="show-hide"
+                                        class="text-black bg-stone-500 hover:text-white cursor-pointer p-1 rounded-md m-2 transition-colors duration-1000">Show
+                                        Details</a>
+                                </div>
+
+                                <h2
+                                    class="text-xl text-center mb-2 p-2 mt-3 mx-auto bg-white text-grey-800 rounded shadow-md w-1/2">
+                                    Subtotal: £{{$basket->total_amount}}
+                                </h2>
+                                <div class="flex justify-center mt-auto w-full">
+                                    <button type="Checkout"
+                                        class="bg-amber text-black hover:text-white my-3 py-2 px-8 rounded-lg hover:underlinehover:text-white w-[80%] font-bold transition-colors duration-1000">
+                                        @csrf
+                                        <a href="{{ route('checkout.checkout') }}">Checkout</a>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    </div>
                     @endif
                     <!--BASKET ITEMS-->
                     <div
-                        class="justify-between h-[90%] w-full mt-5 xl:w-3/4 xl:mt-0 transition-all duration-1000 flex flex-col ">
+                        class="justify-between h-[90%] w-full mt-5 xl:w-3/4 xl:mt-0 transition-all duration-1000 flex flex-col shadow-md">
                         @if(count($basketItems) === 0)
                             <div
-                                class="py-2 a px-4 bg-white text-grey-800 rounded-lg shadow-md h-fit text-center w-full xl:w-[98%] ml-0 xl:ml-4">
+                                class="py-2 a px-4 bg-white dark:bg-yellow-500 text-grey-800 rounded-lg shadow-md h-fit text-center w-full xl:w-[98%] ml-0 xl:ml-4">
                                 No items in basket
                             </div>
                         @else
                             <div
-                                class="overflow-y-auto overflow-x-hidden h-[630px] mb-4 rounded-lg transition-all duration-1000">
+                                class="overflow-y-auto no-scrollbar overflow-x-hidden h-[630px] mb-4 rounded-lg transition-all duration-1000 ">
                                 <ul class="h-full mb-6">
                                     @foreach($basketItems as $basketItem)
                                         <div
-                                            class="bg-white mx-2 rounded-lg flex justify-between text-center items-center p-2 mb-2 w-full">
+                                            class="bg-white dark:bg-yellow-500 mx-2 rounded-lg flex justify-start sm:justify-between text-center items-center p-2 mb-2 w-[96%] xl:w-full shadow-md z-10">
                                             <div class="flex">
-                                            <img class="size-[125px] min-w-[125px] rounded mr-6"
-                                                src="{{ asset('Images/product images/' . $basketItem->product_name . '.png') }}" />
-                                           
-                                            <div class="flex flex-col flex-shrink">
-                                            <p class="text-base xl:text-lg text-center h-fit my-auto text-nowrap">{{$basketItem->product_name}}</p>
-                                            <!--Quantity Controllers -->
-                                            <div class="flex">
-                                            <form action="{{ route('basket.decreaseQuantity') }}" method="post"
-                                                    >
-                                                    @csrf
-                                                    <input type="hidden" name="product_id"
-                                                        value="{{ $basketItem->product_id }}" />
-                                                    <button
-                                                        class="py-1 px-3 bg-white text-grey-800 rounded-lg shadow-md hover:text-amber h-fit"
-                                                        type="submit"><i class="fa-solid fa-minus text-sm"></i></button>
-                                                </form>
-                                                <p class="m-3 h-fit mt-1 text-sm">{{$basketItem->quantity}}</p>
-                                                <!-- increase button -->
-                                                <form action="{{ route('basket.increaseQuantity') }}" method="post"
-                                                    class="flex">
-                                                    @csrf
-                                                    <input type="hidden" name="product_id"
-                                                        value="{{ $basketItem->product_id }}" />
-                                                    <button
-                                                        class="py-1 px-3 bg-white text-grey-800 rounded-lg shadow-md hover:text-amber h-fit"
-                                                        type="submit"><i class="fa-solid fa-plus text-sm"></i></button>
-                                                </form>
+                                                <img class="size-[125px] min-w-[125px] rounded mr-6"
+                                                    src="{{ asset('Images/product images/' . $basketItem->product_name . '.png') }}" />
+
+                                                <div class="flex flex-col flex-shrink">
+                                                    <p class="text-sm xl:text-lg text-center h-fit my-auto ">
+                                                        {{$basketItem->product_name}}</p>
+                                                    <!--Quantity Controllers -->
+                                                    <div class="flex">
+                                                        <form action="{{ route('basket.decreaseQuantity') }}" method="post">
+                                                            @csrf
+                                                            <input type="hidden" name="product_id"
+                                                                value="{{ $basketItem->product_id }}" />
+                                                            <button
+                                                                class="py-1 px-3 bg-white text-grey-800 rounded-lg shadow-md hover:text-amber h-fit"
+                                                                type="submit"><i class="fa-solid fa-minus text-sm"></i></button>
+                                                        </form>
+                                                        <p class="m-3 h-fit mt-1 text-sm">{{$basketItem->quantity}}</p>
+                                                        <!-- increase button -->
+                                                        <form action="{{ route('basket.increaseQuantity') }}" method="post"
+                                                            class="flex">
+                                                            @csrf
+                                                            <input type="hidden" name="product_id"
+                                                                value="{{ $basketItem->product_id }}" />
+                                                            <button
+                                                                class="py-1 px-3 bg-white text-grey-800 rounded-lg shadow-md hover:text-amber h-fit"
+                                                                type="submit"><i class="fa-solid fa-plus text-sm"></i></button>
+                                                        </form>
+                                                    </div>
+
+                                                </div>
                                             </div>
-                                            
-                                            </div>
-                                            </div>
-                                            
+
                                             <div class="flex justify-center items-center p-2">
                                                 <!-- decrease button -->
-                                                
+
                                                 <!-- delete button -->
                                                 <form action="{{ route('basket.remove') }}" method="post" class="ml-4">
                                                     @csrf
@@ -166,12 +171,12 @@
                             <!--BASKET Total + Checkout Button-->
                             <div class="hidden xl:flex xl:flex-col justify-center items-center w-full">
                                 <h2
-                                    class="text-2xl text-center mb-2 p-2 mt-5 mx-3 bg-white text-grey-800 rounded shadow-md w-1/2">
+                                    class="text-2xl text-center mb-2 p-2 mt-5 mx-3 xl:mx-auto bg-white text-grey-800 rounded shadow-md w-1/2">
                                     Subtotal: £{{$basket->total_amount}}
                                 </h2>
                                 <div class="flex justify-center mt-auto w-full">
                                     <button type="Checkout"
-                                        class="bg-yellow-400 text-white mt-5 py-4 px-8 rounded-lg shadow-lg hover:underline dark:bg-stone-900 dark:hover:text-amber w-1/3 font-bold transition-colors duration-1000">
+                                        class="bg-yellow-400 text-white mt-5 py-4 px-8 rounded-lg  hover:underline dark:bg-stone-900 dark:hover:text-amber w-1/3 font-bold transition-colors duration-1000">
                                         @csrf
                                         <a href="{{ route('checkout.checkout') }}">Checkout</a>
                                     </button>
--- resources/views/layouts/addressForm.blade.php
+++ resources/views/layouts/addressForm.blade.php
@@ -4,32 +4,32 @@
                             @csrf
                             @if($address != null)
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="street_address" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">Street
+                                    <label for="street_address" class="xl:w-full mx-auto block text-black dark:text-white  xl:dark:text-black mb-1 transition-colors duration-1000">Street
                                         Address</label>
                                     <input type="text" name="street_address" id="street_address"
                                         value="{{$address->street_address}}" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="city" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">City</label>
+                                    <label for="city" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">City</label>
                                     <input type="text" name="city" id="city" value="{{$address->city}}" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="county" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">County</label>
+                                    <label for="county" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">County</label>
                                     <input type="text" name="county" id="county" value="{{$address->county}}" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="country" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">Country</label>
+                                    <label for="country" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">Country</label>
                                     <input type="text" name="country" id="country" value="{{$address->country}}" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="post_code" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">Post
+                                    <label for="post_code" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">Post
                                         Code</label>
                                     <input type="text" name="post_code" id="post_code" value="{{$address->post_code}}"
-                                        required class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        required class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <input type="hidden" name="type" value="shipping" />
                                 <div class="flex justify-center mt-auto">
@@ -39,31 +39,31 @@
                                 </div>
                             @else
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="street_address" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">Street
+                                    <label for="street_address" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">Street
                                         Address</label>
                                     <input type="text" name="street_address" id="street_address" value="Street Address"
-                                        required class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        required class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="city" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">City</label>
+                                    <label for="city" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">City</label>
                                     <input type="text" name="city" id="city" value="City" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="county" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">County</label>
+                                    <label for="county" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">County</label>
                                     <input type="text" name="county" id="county" value="County" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="country" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">Country</label>
+                                    <label for="country" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">Country</label>
                                     <input type="text" name="country" id="country" value="Country" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <div class="flex flex-col items-start mb-1 xl:mb-4">
-                                    <label for="post_code" class="xl:w-full mx-auto block text-stone-600 dark:text-black mb-1 transition-colors duration-1000">Post
+                                    <label for="post_code" class="xl:w-full mx-auto block text-black dark:text-white xl:dark:text-black mb-1 transition-colors duration-1000">Post
                                         Code</label>
                                     <input type="text" name="post_code" id="post_code" value="Post Code" required
-                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-300 transition-colors duration-1000" />
+                                        class="w-[90%] xl:w-full mx-auto p-3 mb-1 xl:mb-4 border border-gray-300 rounded dark:bg-stone-200 transition-colors duration-1000" />
                                 </div>
                                 <input type="hidden" name="type" value="shipping" />
                                 <div class="flex justify-center mt-auto">
