89ab6cd49b765a43a13a2b712f7dab34d85c351e
Aryanjot Grewal <aryanzgrewal@gmail.com>
2025-03-16 01:28:04 +0000

    Added memorable info to the signup form and users table.

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Models\Users;
+use App\Models\User;
 
 class SignupController extends Controller{
 
@@ -20,16 +22,20 @@ class SignupController extends Controller{
             'phone_number' => 'required|string|max:12',
             'email_address' => 'required|string|email|max:255',
             'password' => 'required|string|min:8|confirmed',
+            'memorable_question' => 'required|string|max:255',
+            'memorable_answer' => 'required|string|max:55',
         ]);
 
 
     
-        Users::create([
+        User::create([
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'phone_number' => $request->phone_number,
             'email_address' => $request->email_address,   
             'password' => bcrypt($request->password),
+            'memorable_question' => $request->memorable_question,
+            'memorable_answer' => Hash::make($request->memorable_answer),
         ]);
 
         return redirect()->route('login')->with('success', 'Signup successful!');
