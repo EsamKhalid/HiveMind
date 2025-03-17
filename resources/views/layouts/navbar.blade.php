41bab8326d4d4d90aae71bf086bdc650a82b9c6f
Taran C <t.chana.21.101@gmail.com>
2025-03-17 12:40:09 +0000

    partial fix for navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
+
 <head>
     <script>
-        (function () {
+        (function() {
             // Check for saved theme or system preference and apply it
             const currentTheme = localStorage.getItem("theme");
             const deviceTheme = window.matchMedia(
@@ -13,43 +14,36 @@
                 document.documentElement.classList.remove("dark");
             }
         })();
-        
-function toggleTheme(){
-    var currentTheme = localStorage.getItem("theme");
 
-    if(!currentTheme){
-        localStorage.setItem("theme","dark");
-    }else if(currentTheme == "dark"){
-        localStorage.setItem("theme","light");
-    }else{
-        localStorage.setItem("theme","dark");
-    }
-    document.documentElement.classList.toggle("dark");
-}
-document.addEventListener('DOMContentLoaded',(e)=>{
-   for(let E of document.getElementsByClassName('transition-none')){
-    E.classList.remove('transition-none')
-   } 
-});
-</script>
+        function toggleTheme() {
+            var currentTheme = localStorage.getItem("theme");
+
+            if (!currentTheme) {
+                localStorage.setItem("theme", "dark");
+            } else if (currentTheme == "dark") {
+                localStorage.setItem("theme", "light");
+            } else {
+                localStorage.setItem("theme", "dark");
+            }
+            document.documentElement.classList.toggle("dark");
+        }
+        document.addEventListener('DOMContentLoaded', (e) => {
+            for (let E of document.getElementsByClassName('transition-none')) {
+                E.classList.remove('transition-none')
+            }
+        });
+    </script>
 </head>
+
 <body class="transition-none transition-all ease-in-out duration-1000">
     <div class="overflow-hidden">
         <!-- navbar -->
         <nav
-            class="navbar flex justify-between items-center bg-amber dark:bg-stone-800 text-stone-800 dark:text-yellow-100 font-bold px-6 py-4 transition-colors duration-1000"
-        >
+            class="navbar flex justify-between items-center bg-amber dark:bg-stone-800 text-stone-800 dark:text-yellow-100 font-bold px-6 py-4 transition-colors duration-1000">
             <!-- logo section -->
 
-            <a
-                href="{{ route('home') }}"
-                class="flex items-center hover:text-yellow-100 duration-200"
-            >
-                <img
-                    src="{{ asset('../Images/HiveMind Logo.png') }}"
-                    alt="HiveMind Logo"
-                    class="h-12 w-auto mr-3"
-                />
+            <a href="{{ route('home') }}" class="flex items-center hover:text-yellow-100 duration-200">
+                <img src="{{ asset('../Images/HiveMind Logo.png') }}" alt="HiveMind Logo" class="h-12 w-auto mr-3" />
                 <h1 class="text-2xl font-bold">HiveMind</h1>
             </a>
 
@@ -59,82 +53,54 @@ document.addEventListener('DOMContentLoaded',(e)=>{
                 <!-- links are next to icons -->
                 <ul class="hidden lg:flex-row-reverse space-x-8 text-lg">
                     <li>
-                        <a
-                            class="fa-solid fa-lightbulb text-2xl m-auto hover:cursor-pointer dark:hover:text-amber"
-                            onclick="toggleTheme()"
-                        ></a>
+                        <a class="fa-solid fa-lightbulb text-2xl m-auto hover:cursor-pointer dark:hover:text-amber"
+                            onclick="toggleTheme()"></a>
                     </li>
                     <li>
-                        <a
-                            href="{{ route('contact') }}"
-                            class="hidden lg:flex hover:text-yellow-100 dark:hover:text-amber"
-                            >Contact Us</a
-                        >
+                        <a href="{{ route('contact.view') }}"
+                            class="hidden lg:flex hover:text-yellow-100 dark:hover:text-amber">Contact Us</a>
                     </li>
-                    
-                    
+
+
                     <li>
-                        <a
-                            href="{{ route('about') }}"
-                            class="hidden lg:flex hover:text-yellow-100 dark:hover:text-amber mr-5"
-                            >About Us</a
-                        >
+                        <a href="{{ route('about') }}"
+                            class="hidden lg:flex hover:text-yellow-100 dark:hover:text-amber mr-5">About Us</a>
                     </li>
-                    
+
                     <li>
-                        <a
-                            href="{{ route('products') }}"
-                            class="hidden lg:flex hover:text-yellow-100 dark:hover:text-amber"
-                            >Shop Us</a
-                        >
+                        <a href="{{ route('products') }}"
+                            class="hidden lg:flex hover:text-yellow-100 dark:hover:text-amber">Shop Us</a>
                     </li>
 
                 </ul>
-                <a class="fa-solid fa-lightbulb text-2xl m-auto hover:cursor-pointer hover:text-yellow-100 dark:hover:text-amber" onclick="toggleTheme()"></a>
+                <a class="fa-solid fa-lightbulb text-2xl m-auto hover:cursor-pointer hover:text-yellow-100 dark:hover:text-amber"
+                    onclick="toggleTheme()"></a>
                 <!-- search icon // trying to make it so that search bar appears when user clicks on icon (still a wip) -->
                 <form action="{{ route('products') }}" method="get">
                     <div class="flex items-center space-x-4">
                         <!-- search bar -->
-                        <input
-                            type="text"
-                            name="search"
-                            placeholder="Search"
-                            class="hidden lg:block w-48 bg-white text-stone-800 placeholder-stone-500 border-black rounded-full px-4 py-2 shadow-lg focus:outline-none focus:ring-2 focus:ring-white dark:text-stone-800"
-                        />
+                        <input type="text" name="search" placeholder="Search"
+                            class="hidden lg:block w-48 bg-white text-stone-800 placeholder-stone-500 border-black rounded-full px-4 py-2 shadow-lg focus:outline-none focus:ring-2 focus:ring-white dark:text-stone-800" />
 
                         <!-- search icon -->
-                        <button
-                            type="submit"
+                        <button type="submit"
                             class="text-stone-800 dark:text-yellow-100 dark:hover:text-amber transition-colors duration-1000"
-                            name="productSearchIcon"
-                        >
-                            <i
-                                class="fas fa-search fa-xl dark:hover:text-amber"
-                            ></i>
+                            name="productSearchIcon">
+                            <i class="fas fa-search fa-xl dark:hover:text-amber"></i>
                         </button>
                     </div>
                 </form>
-                <a
-                    href="{{ route('basket.view') }}"
-                    class="fas fa-shopping-basket fa-xl dark:hover:text-amber hover:text-yellow-100 "
-                ></a>
+                <a href="{{ route('basket.view') }}"
+                    class="fas fa-shopping-basket fa-xl dark:hover:text-amber hover:text-gray-200"></a>
+                @auth
+                    <a href="{{ route('account') }}"
+                        class="fas fa-user fa-xl hover:text-gray-200 transition dark:hover:text-amber"></a>
                 @else
-                <a
-                    href="{{ route('login') }}"
-                    class="fas fa-shopping-basket fa-xl hover:text-yellow-100 dark:hover:text-amber"
-                ></a>
-                @endauth @auth
-                <a
-                    href="{{ route('account') }}"
-                    class="fas fa-user fa-xl hover:text-yellow-100 transition dark:hover:text-amber"
-                ></a>
-                @else
-                <a
-                    href="{{ route('login') }}"
-                    class="fas fa-user fa-xl hover:text-yellow-100 transition dark:hover:text-amber"
-                ></a>
+                    <a href="{{ route('login') }}"
+                        class="fas fa-user fa-xl hover:text-gray-200 transition dark:hover:text-amber"></a>
                 @endauth
             </div>
-        </nav>
+    </div>
+    </nav>
     </div>
 </body>
