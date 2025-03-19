6d8f0449aadf67e5a8ccbe6d687e9bb7e271a275
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-03-19 04:18:06 +0000

    Based stats page off user management instead of enquiries to make it match the rest of the admin pages and added stats route to the sidebar

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -2,27 +2,22 @@
 <html lang="en">
     <head>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <title>User Enquiries</title>
+        <title>Dashboard</title>
     </head>
-    <body>
-        @include('layouts.navbar')
-        <div class="flex justify-center">
-            @if($errors->any())
-            <h4 class="text-3xl">{{$errors->first()}}</h4>
-            @endif
-        </div>
 
-        <main class="flex justify-center items-center text-center min-h-screen">
-            <div class="w-full max-w-4xl p-4">
-                <p
-                    class="text-7xl text-white p-5 bg-yellow-400 dark:bg-gray-400 dark:bg-opacity-40 rounded-md my-10"
-                >
+    <body class="bg-white dark:bg-stone-950 flex">
+        @include('layouts.sidebar') 
+
+        <div class="flex justify-center overflow-x-auto">
+            <div class="">
+                <p class="text-7xl text-white p-5 bg-yellow-400 dark:bg-gray-400 dark:bg-opacity-40 rounded-md my-10">
                     <i class="fa-solid fa-chart-line text-7xl mr-4 my-auto"></i>
                     Statistics
-                </p>
-            </div>
-        </main>
+                </p>  
+                
 
-        @include('layouts.footer')
+
+  
+
     </body>
 </html>
