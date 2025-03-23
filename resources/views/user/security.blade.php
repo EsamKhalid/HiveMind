b31ce2f5613bd2847269230850f01a4aa9371802
Aryanjot Grewal <aryanzgrewal@gmail.com>
2025-03-23 15:37:19 +0000

    Added dark mode for security settings

--- resources/views/user/security.blade.php
+++ resources/views/user/security.blade.php
@@ -8,10 +8,11 @@
 
     <main>
 
-        <a href="{{ route('user.settings') }}" class="fas fa-arrow-left fa-3x pl-4 pt-2"></a>
+        <a href="{{ route('user.settings') }}" 
+            class="fas fa-arrow-left fa-3x pl-4 pt-2 dark:text-amber transition-colors ease-in-out duration-1000"></a>
 
-        <div class="xl:w-[40%] lg:w-[50%] md:w-[60%] sm:w-[90%] max-w-full mx-auto mt-2 mb-20 p-8 bg-white rounded-lg shadow-md">
-            <h1 class="text-4xl font-bold mb-6">Security Settings</h1>
+        <div class="xl:w-[40%] lg:w-[50%] md:w-[60%] sm:w-[90%] max-w-full mx-auto mt-2 mb-20 p-8 bg-white rounded-lg shadow-md dark:bg-stone-700 transition-colors ease-in-out duration-1000">
+            <h1 class="text-4xl font-bold mb-6 dark:text-yellow-400 transition-colors ease-in-out duration-1000">Security Settings</h1>
 
             @if(session('success'))
                 <div class="mb-4 p-3 bg-green-100 text-green-700 rounded">
@@ -32,13 +33,13 @@
             <form action="{{ route('settings.security.update') }}" method="POST" class="space-y-4">
                 @csrf
                 <div>
-                    <label class="block text-sm font-semibold">Current Password</label>
+                    <label class="block text-sm font-semibold dark:text-yellow-400 transition-colors ease-in-out duration-1000">Current Password</label>
                     <input type="password" name="current_password" required
                         class="w-full px-4 py-2 border rounded-lg focus:ring-yellow-500 focus:border-yellow-500">
                 </div>
 
                 <div>
-                    <label class="block text-sm font-semibold">Select a New Memorable Question</label>
+                    <label class="block text-sm font-semibold dark:text-yellow-400 transition-colors ease-in-out duration-1000">Select a New Memorable Question</label>
                     <select name="memorable_question" required
                         class="w-full px-4 py-2 border rounded-lg focus:ring-yellow-500 focus:border-yellow-500">
                         <option value="" disabled selected hidden>Select a question</option>
@@ -53,7 +54,7 @@
                 </div>
 
                 <div class="pb-6">
-                    <label class="block text-sm font-semibold">New Memorable Answer</label>
+                    <label class="block text-sm font-semibold dark:text-yellow-400 transition-colors ease-in-out duration-1000">New Memorable Answer</label>
                     <input type="text" name="memorable_answer" required
                         class="w-full px-4 py-2 border rounded-lg focus:ring-yellow-500 focus:border-yellow-500">
                 </div>
